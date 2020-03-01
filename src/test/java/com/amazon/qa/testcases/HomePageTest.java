38219c4f52b134b511e1aed7e9c42aaf63f30bd3
Deepak Malik <deepak@niyotail.com>
2020-03-01 21:08:56 +0530

    extent reporting and custom log implemented

--- src/test/java/com/amazon/qa/testcases/HomePageTest.java
+++ src/test/java/com/amazon/qa/testcases/HomePageTest.java
@@ -10,6 +10,25 @@ import com.amazon.qa.base.BaseClass;
 import com.amazon.qa.pages.HomePage;
 import com.amazon.qa.pages.WomenShoes;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/*public static Logger createLogger()  {
+	  Logger aLogger = LogManager.getRootLogger();
+	  aLogger.debug("\n" + "Inside createLogger - Logger is being set up. New test setup beginning.");
+	  verificationErrors = new StringBuffer();
+	  aLogger.info("Logger has been set up.");
+	  return aLogger;
+	 }
+
+	 protected static void createLogFile (FirefoxProfile fp) throws Exception {
+	  File outfile = new File(outfileName);
+	  if (!outfile.exists())  
+	   outfile.createNewFile();
+	  fp.setPreference("webdriver.log.driver", "DEBUG");
+	  fp.setPreference("webdriver.log.file", outfileName);
+	 }
+*/
 public class HomePageTest extends BaseClass{
 	
 	WomenShoes womenShoes;
@@ -17,6 +36,9 @@ public class HomePageTest extends BaseClass{
 	
 	HomePageTest() throws FileNotFoundException{
 		super();
+		
+		//Logger log = Logger.getLogger("HomePageTest");
+		//PropertyConfigurator.configure("log4j2.properties");
 	}
 	
 	@BeforeMethod
